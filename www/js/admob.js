de69718518f93e8ebb96f3a347f0acf4a69ee710
Evgeniy Gribkov <grandcazino@gmail.com>
2018-09-20 21:49:00 +0300

    ad - fix

--- www/js/admob.js
+++ www/js/admob.js
@@ -22,7 +22,7 @@ function initApp() {
   AdMob.createBanner( {
     adId: admobid.banner,
     position: AdMob.AD_POSITION.BOTTOM_CENTER,
-    isTesting: true, // TODO: remove this line when release
+    //isTesting: true, // TODO: remove this line when release
     overlap: false,
     offsetTopBar: false,
     bgColor: 'black'
@@ -31,7 +31,7 @@ function initApp() {
   // this will load a full screen ad on startup
   AdMob.prepareInterstitial({
     adId: admobid.interstitial,
-    isTesting: true, // TODO: remove this line when release
+    //isTesting: true, // TODO: remove this line when release
     autoShow: true
   });
 }
